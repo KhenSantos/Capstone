ab5c0e9de625835e376ce54733e34ad4f1dec8df
KhenSantos <aljoncoronado@gmail.com>
2025-02-19 16:13:10 +0800

    initial commit

---
+++ Home.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pandas as pd
+import matplotlib as plt
+import seaborn as sns
+
+sns.set()
+
+st.title("CAPSTONE PROJECT®")
+st.subheader("It is Always Fun to Learn!")
+st.write("Join Us!!")
+
+st.divider()
+st.image("Capstone.jfif", "Welcome!")
